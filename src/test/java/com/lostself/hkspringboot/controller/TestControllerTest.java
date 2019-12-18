5d5905f2190597665c8c0bb62bb2fdce63e4354a
Snowman12138 <Snowman12138@163.com>
2019-12-18 14:24:19 +0800

    ddfsdf

---
+++ src/test/java/com/lostself/hkspringboot/controller/TestControllerTest.java
@@ -0,0 +1,20 @@
+package com.lostself.hkspringboot.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestControllerTest {
+
+    @Test
+    public void getUser() {
+    }
+
+    @Test
+    public void getAllUser() {
+    }
+
+    @Test
+    public void getAllUser2() {
+    }
+}
